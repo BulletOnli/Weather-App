cac8a92bfcccaa6b2d9ff07eae6e0dab1cf79ece
Gemmuel <gemmueldelacruz@gmail.com>
2022-11-01 21:20:10 +0800

    major changes

--- index.js
+++ index.js
@@ -13,23 +13,19 @@ async function manilaWeather() {
         if (!response.ok) throw new Error(err)
         const data = await response.json()
 
-        const { feels_like, humidity, temp, pressure } = data.main
+        const { humidity, temp } = data.main
         const wind = data.wind.speed
         const cityName = data.name
         const country = data.sys.country
-
         const getCelsius = Math.ceil(temp - 273.15)
-        const getFahren = Math.ceil((data.main.temp - 273.15) * (9/5) + 32)
-        const feelsLike = Math.ceil(feels_like - 273.15)
+        const getFahren = Math.ceil((temp - 273.15) * (9/5) + 32)
         
         document.querySelector('.city').textContent = `${cityName}`
         document.querySelector('.country').textContent = country
         document.querySelector('.celsius').textContent = `${getCelsius}°C`
         document.querySelector('.fahren').textContent = `${getFahren}°F`
-        document.querySelector('.feels').textContent = `${feelsLike}°C`
         document.querySelector('.humidity').textContent = `${humidity}%`
         document.querySelector('.wind').textContent = `${wind} km/h`
-        document.querySelector('.pressure').textContent = `${pressure} hPa`
 
         console.log(data)
     } catch (err) {
