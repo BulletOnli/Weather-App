58c961fe601c9e879926d8f53052535fc95055fa
Gemmuel <gemmueldelacruz@gmail.com>
2022-11-02 14:05:08 +0800

    removed unnecessary code

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import inputCity from "./components/app.js"
+checkWeather('Manila') // Default weather
+
+const input = document.getElementById('input')
 
 document.getElementById('search').addEventListener('click', () => {
-    inputCity()
+    checkWeather(input.value)
     input.value = ''
     document.querySelector('.gridContainer').style.display = 'grid'
 })
 
-manilaWeather()
-async function manilaWeather() {
+async function checkWeather(e) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manila&appid=af2d62810fab0531012423f0fd639cdc`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e}&appid=af2d62810fab0531012423f0fd639cdc`)
         if (!response.ok) throw new Error(err)
         const data = await response.json()
 
@@ -17,8 +18,9 @@ async function manilaWeather() {
         const wind = data.wind.speed
         const cityName = data.name
         const country = data.sys.country
+
         const getCelsius = Math.ceil(temp - 273.15)
-        const getFahren = Math.ceil((temp - 273.15) * (9/5) + 32)
+        const getFahren = Math.ceil((data.main.temp - 273.15) * (9/5) + 32)
         
         document.querySelector('.city').textContent = `${cityName}`
         document.querySelector('.country').textContent = country
@@ -29,7 +31,8 @@ async function manilaWeather() {
 
         console.log(data)
     } catch (err) {
-        document.querySelector('.city').textContent = `Error`
+        document.querySelector('.city').textContent = `Invalid City`
+        document.querySelector('.country').textContent = ''
         document.querySelector('.celsius').textContent = ''
         document.querySelector('.fahren').textContent = ''
 
